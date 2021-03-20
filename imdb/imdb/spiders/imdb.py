67f511e81b006e7b7447e3ad1dd084f9bb4b52b4
Tamas Szabados <tszabad@gmail.com>
2021-03-20 19:15:21 +0100

    movie scraping app is ready

--- imdb/imdb/spiders/imdb.py
+++ imdb/imdb/spiders/imdb.py
@@ -3,34 +3,32 @@ import scrapy
 
 class ImdbMovieSpider(scrapy.Spider):
     name = 'imdb_movie'
-    allowed_domains = ['https://www.imdb.com']
+    allowed_domains = ['imdb.com']
     start_urls = ['https://www.imdb.com/chart/top/?ref_=nv_mv_500']
     
     def parse(self, response):
         print("procesing:"+response.url)
-        for i in range(1,10):
-            link = f"//tr[{i}]/td[@class='titleColumn' and 2]/a[1]/@href"
-            text = f"//tr[{i}]/td[@class='titleColumn' and 2]/a[1]/text()"
-            movie = response.xpath(text).get()
-            next_path = response.xpath(link).get()
-            if next_path:
-                yield scrapy.Request(response.urljoin(next_path), callback=self.parse)
-            print(response.url)
-            second_path = response.xpath("//a[contains(.,'See full cast')]/@href").get()
-            if second_path:
-                yield scrapy.Request(response.urljoin(second_path), callback=self.parse)
-            print(response.url)
-            cast = response.xpath("//tr[@class='odd']/td[2]/a[1]/text()").getall()
-            
-            if movie is not None and cast is not None:
-                yield {
-                    "movie": movie,
-                    "cast": cast
+        for i in range(1,150):
+            link_selector = f"//tr[{i}]/td[@class='titleColumn' and 2]/a[1]/@href"
+            movie_link = response.xpath(link_selector).getall()
+            for link in movie_link:
+                yield scrapy.Request(response.urljoin(link), callback = self.parse_link)
+    
+    def parse_link(self, response):
+            print("procesing:"+response.url)
+            cast_link = response.xpath("//a[contains(.,'See full cast')]/@href").get()
+            yield scrapy.Request(response.urljoin(cast_link), callback = self.parse_cast)
+
+    def parse_cast(self, response):
+            print("procesing:"+response.url)
+            title = response.xpath("//h3/a[1]/text()").get()
+            cast = response.xpath("//td[2]/a[1]/text()").getall()
+            yield {
+                "title": title,
+                "cast": cast
                     }
 
-            final_path = 'https://www.imdb.com/chart/top/?ref_=nv_mv_500'
-            if final_path:
-                yield scrapy.Request(response.urljoin(final_path), callback=self.parse)
+            
             
 
            
